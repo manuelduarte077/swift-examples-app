18aec04f1607ea31fb158f6463b4eab68d0ca81a
Manuel Duarte <carlosd99m@gmail.com>
2022-04-09 01:22:33 -0600

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Manuel Duarte on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
