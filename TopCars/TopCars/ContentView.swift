cb0f613ecc629104f61199d0fb7208265b6cece4
Manuel Duarte  <carlosd99m@gmail.com>
2023-04-15 23:40:52 -0600

    wip: initial code

---
+++ TopCars/TopCars/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TopCars
+//
+//  Created by Don Manuel on 15/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TopCars/TopCars/TopCarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopCarsApp.swift
+//  TopCars
+//
+//  Created by Don Manuel on 15/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TopCarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
