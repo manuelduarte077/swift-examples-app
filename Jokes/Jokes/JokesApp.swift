24f4a14186ae17f222c11df7708c2a9fcbe85138
Manuel Duarte  <carlosd99m@gmail.com>
2023-04-16 21:28:01 -0600

    fix: jokes card

---
+++ Jokes/Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Don Manuel on 16/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
