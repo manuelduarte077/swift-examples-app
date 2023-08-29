2e8a5cc0518a195b963d2f45008488142f4ac3a0
Manuel Duarte  <carlosd99m@gmail.com>
2023-08-29 00:40:20 -0600

    first app uikit

---
+++ myApp/myApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  myApp
+//
+//  Created by Don Manuel on 28/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
