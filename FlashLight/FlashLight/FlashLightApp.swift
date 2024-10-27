19c03c9321be8c4d42e3a93c06f4ee30ca735ba0
Manuel Duarte <carlosd99m@gmail.com>
2024-10-27 17:14:22 -0600

    ✨ ADD: FlashLight Control

---
+++ FlashLight/FlashLight/FlashLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashLightApp.swift
+//  FlashLight
+//
+//  Created by Manuel Duarte on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
