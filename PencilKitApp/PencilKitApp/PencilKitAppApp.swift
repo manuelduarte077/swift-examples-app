7e1cbcc7fb7a7f73c604e6e34fb022e2b0c0ede0
manuelduarte077 <carlosd99m@gmail.com>
2025-04-02 12:15:44 -0600

    feat: Add initial PencilKit app structure and assets

---
+++ PencilKitApp/PencilKitApp/PencilKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitAppApp.swift
+//  PencilKitApp
+//
+//  Created by Manuel Duarte on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
